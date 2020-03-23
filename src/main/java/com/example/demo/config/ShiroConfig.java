b5ea47a72a3c62c8fa8b96f3bf31bf54512539e1
wuzhanhao <1282591110@qq.com>
2020-03-24 00:07:29 +0800

    shiro,认证

--- src/main/java/com/example/demo/config/ShiroConfig.java
+++ src/main/java/com/example/demo/config/ShiroConfig.java
@@ -1,6 +1,18 @@
 package com.example.demo.config;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.testng.annotations.Test;
 
 /**
  * 2020/3/18
@@ -16,6 +28,34 @@ import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
  *      @RequiresPermissions
  *     要求subject中必须同时含有file:read和write:aFile.txt的权限才能执行方法someMethod()。否则抛出异常AuthorizationException。
  */
-public class ShiroConfig extends HashedCredentialsMatcher{
+@Component
+@Configuration
+public class ShiroConfig{
+
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+    @Before
+    public void loginUser(){
+        simpleAccountRealm.addAccount("wzh","666");
+    }
+
+    @Test
+    public void testAuthentication() {
+        //创建SecurityManager
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        SecurityUtils.setSecurityManager(securityManager);
+        securityManager.setRealm(simpleAccountRealm);
+        //主题提交认证
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("wzh", "222");
+        subject.login(token);
+
+        boolean authenticated = subject.isAuthenticated();
+        System.out.println(authenticated);
+
+        subject.logout();
+        System.out.println(authenticated);
+
+    }
 
 }
